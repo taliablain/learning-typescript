6e97a3a176d066248165f7d069a397e44fc3bf24
talia blain <talia.blain@digital.homeoffice.gov.uk>
2025-06-16 17:30:34 +0100

    formatting and compiling

--- Unit1.js
+++ Unit1.js
@@ -1,3 +1,27 @@
-function greet(name) {
+"use strict";
+const barcodeOne = 'ABCD';
+const uppercasedBarcodeOne = barcodeOne.toUpperCase();
+const barcodeTwo = 1;
+const uppercasedBarcodeTwo = barcodeTwo.toUpperCase();
+function printProduct(product) {
+    console.log(product.title);
+}
+printProduct({ id: 1, title: "Mouse" });
+let age = 25;
+let names = "Talia";
+let isAdmin = false;
+function greetTs(name) {
     return `Hello ${name}`;
 }
+greetTs("Talia");
+greetTs(42);
+function add(a, b) {
+    return a + b;
+}
+let city = "Paris"; //inferred as string 
+let data = "something";
+data = 42; //allowed
+function log(message) {
+    console.log(message);
+}
+//difference between greet in ts and js 
